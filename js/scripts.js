55962b65b29b996c9eb9b0d1e9b16e0fd330b78e
Student <example@example.com>
2019-01-31 16:51:18 -0800

    begin to add a turn property to check which player is active

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ function Player(name) {
   this.roundScore = 0;
   this.roll = 0;
   this.round = 1
+  this.turn = true;
 }
 
 Player.prototype.rollDice = function() {
@@ -28,16 +29,12 @@ Player.prototype.checkWin = function(totalScore) {
   }
 }
 
-Player.prototype.nextRound = function() {
-  this.round += 1;
-}
-
 
 Player.prototype.holdScore = function(){
   this.totalScore += this.roundScore;
   this.roundScore = 0;
   this.checkWin(this.totalScore);
-  this.roundCheck();
+  this.myTurn();
   incrementRound();
 }
 
@@ -50,7 +47,7 @@ Player.prototype.getRoundScore = function() {
     } else {
       this.roundScore = 0;
       this.roll = myRoll;
-      this.roundCheck();
+      this.myTurn();
       incrementRound();
       }
     }
@@ -63,21 +60,13 @@ Player.prototype.showResults = function (){
   $("#roll").text(this.roll);
 }
 
-Player.prototype.roundCheck = function () {
-  if (this.round % 2 !== 0) {
-    player1.showResults();
-    // $(".p1").toggle();
-    // $(".p2").toggle();
-    console.log("ODD " + " if statement")
-  // return false
-} else  {
-  player2.showResults();
-  // $(".p1").toggle();
-  // $(".p2").toggle();
-  console.log("EVEN " + " else statement");
-  //  return true;
+Player.prototype.myTurn = function () {
+  if (this.turn === true) {
+    this.turn = false;
+  } else {
+    this.turn = true;
+    }
   }
-}
 
 
 
@@ -94,37 +83,37 @@ function incrementRound() {
   }
 }
 
-function rollButton() { //not working as of yet
-  $("#roll1").off();
-  $("#roll1").click(function(){
-    this.getRoundScore();
-  })
-}
+// function rollButton() { //not working as of yet
+//   $("#roll1").off();
+//   $("#roll1").click(function(){
+//     this.getRoundScore();
+//   })
+// }
 
 $(document).ready(function() {
-  rollButton();
+});
   //$("#roll1").click(function() {
   //  $("#round").text("Round: " + player1.round)
     //player1.getRoundScore();
   //  $("#round-score").text("Round Score: " + player1.roundScore);
   //  $("#roll").text("Roll Value: " + player1.roll)
-  });
-  $("#hold1").click(function() {
-    player1.holdScore();
-  //  $("#round").text("Round: " + player1.round)
-  //  $("#score").text("Total Score: " + player1.totalScore);
-  //  $("#round-score").text("Round Score: " + player1.roundScore);
-  });
-  $("#roll2").click(function() {
-  //  $("#round2").text("Round: " + round)
-    player2.getRoundScore();
-  //  $("#round-score2").text("Round Score: " + player2.roundScore);
-  //  $("#roll-2").text("Roll Value: " + player2.roll)
-  });
-  $("#hold2").click(function() {
-    player2.holdScore();
-  //  $("#round2").text("Round: " + round)
+  // });
+  // $("#hold1").click(function() {
+  //   player1.holdScore();
+  // //  $("#round").text("Round: " + player1.round)
+  // //  $("#score").text("Total Score: " + player1.totalScore);
+  // //  $("#round-score").text("Round Score: " + player1.roundScore);
+  // });
+  // $("#roll2").click(function() {
+  // //  $("#round2").text("Round: " + round)
+  //   player2.getRoundScore();
+  // //  $("#round-score2").text("Round Score: " + player2.roundScore);
+  // //  $("#roll-2").text("Roll Value: " + player2.roll)
+  // });
+  // $("#hold2").click(function() {
+  //   player2.holdScore();
+  // //  $("#round2").text("Round: " + round)
   //  $("#score2").text("Total Score: " + player2.totalScore);
-  //  $("#round-score2").text("Round Score: " + player2.roundScore);
-  });
-//});
+//   //  $("#round-score2").text("Round Score: " + player2.roundScore);
+// });
+// });
